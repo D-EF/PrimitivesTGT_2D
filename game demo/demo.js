ae89b172f9a6708628dd76c62bcf06c52fc5e519
xx <xx.com>
2021-05-19 18:10:23 +0800

    d

--- game demo/demo.js
+++ game demo/demo.js
@@ -10,10 +10,21 @@ canvas.height=800;
 var ctext=canvas.getContext("2d");
 
 var ppg=[];
+
 var base=new CanvasArcTGT(0,0,80,0,1*pi);
+
+/**
+ * @type {Number} 0-2 全局旋轉弧度 單位pi
+ */
+var absRotate=0;
+var baseM22t=new Matrix2x2T();
+function createRotateMatrix(){
+    return baseM22t.rotate(absRotate*pi);
+}
+
 base.setTransformMatrix(new Matrix2x2T().translate(250,250).rotate(0.5*pi));
 
-base.fillStyle="#000"
+base.fillStyle="#000";
 base.render(ctext);
 
 /**
@@ -24,3 +35,13 @@ function setPPG(){
     ppg.push(tempTGT)
 }
 
+class PPG{
+    /**
+     * 游戲中的碰撞單位
+     * @param {Number} rotate 
+     */
+    constructor(rotate){
+        this.rotateBase=new Matrix2x2T().rotate(rotate);
+        this.tgt=new CanvasArcTGT(0,200,20,0,2*pi)
+    }
+}
